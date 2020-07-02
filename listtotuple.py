d140a85c2b3b8f8685d6b33bf8b2f6357f412468
Akshay c hangaragi <52773592+Akshay223@users.noreply.github.com>
2020-07-02 22:38:13 +0530

    Add files via upload

---
+++ listtotuple.py
@@ -0,0 +1,5 @@
+myList = [2, 3, 4, 6, 8, 9]
+
+myTuple = tuple(myList)
+
+print("List converted to Tuple: ", myTuple)
---
+++ tupletostring.py
@@ -0,0 +1,5 @@
+myT = ('b', 'u', 'r', 'g', 'e', 'r')
+myStr = ''.join(myT)
+print("Tuple: ", myT)
+print("Tuple to String: ", myStr)
+© 2020 GitHub, Inc.
